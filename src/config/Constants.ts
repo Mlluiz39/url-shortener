4fef02ffec3af13192df81ec1d67cdc7e61ed14b
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-10-03 17:00:22 -0300

    remove credentials

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:3000',
   MONGO_CONNECTION:
-  'mongodb+srv://mlluiz39:Julia2912@cluster0.bqnsezb.mongodb.net/?retryWrites=true&w=majority'
+  'mongodb+srv://<username>:<password>@cluster0.bqnsezb.mongodb.net/?retryWrites=true&w=majority'
 }
