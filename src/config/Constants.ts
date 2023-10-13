f3a25390a2f11ec0abfbcaa09754eec328b53e80
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-10-13 11:15:56 -0300

    add new url

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://url-shortener-y421.onrender.com/',
   MONGO_CONNECTION:'mongodb+srv://mlluizpereira39:mlluiz29@cluster0.chytyyc.mongodb.net/?retryWrites=true&w=majority'
 }
